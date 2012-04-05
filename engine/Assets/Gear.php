e139b7cda494465042c06088fa3762f873d4cff5
CuamckuyKot <admin@cogear.ru>
2012-04-05 22:33:35 +0400

    Make standalone Assets gear.

---
+++ engine/Assets/Gear.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Assets gear
+ *
+ * @author		Dmitriy Belyaev <admin@cogear.ru>
+ * @copyright		Copyright (c) 2012, Dmitriy Belyaev
+ * @license		http://cogear.ru/license.html
+ * @link		http://cogear.ru
+ * @package		Core
+ * @subpackage          
+ * @version		$Id$
+ */
+class Assets_Gear extends Gear {
+
+    protected $name = 'Assets';
+    protected $description = 'Manage assets';
+    protected $order = -1000;
+    
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->adapter = new Assets_Harvester();
+    }
+}
