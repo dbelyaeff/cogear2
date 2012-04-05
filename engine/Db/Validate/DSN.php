cfcfd54e674be6e2af744bfb1f2581a4a98ebe46
CuamckuyKot <admin@cogear.ru>
2012-04-05 22:38:50 +0400

    Update Db DSN validator.

--- engine/Db/Validate/DSN.php
+++ engine/Db/Validate/DSN.php
@@ -22,10 +22,7 @@ class Db_Validate_DSN extends Form_Validate_Abstract {
      */
     public function validate($value = NULL, $type = 0) {
         if ($value OR $type) {
-            $db_is_active = !is_null(cogear()->db);
-            $db_is_active OR cogear()->activate('db');
-            if (!cogear()->db->checkDSN($value)){
-                $db_is_active OR cogear()->deactivate('db');
+            if (!cogear()->Db->checkDSN($value)){
                 $this->element->addError(t('Look at the database errors and fix them.', 'Db.errors'));
                 return FALSE;
             }
