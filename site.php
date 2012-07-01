bbd61448c4dc280c544f55bda299e716b037b651
CuamckuyKot <admin@cogear.ru>
2012-07-01 12:34:15 +0400

    Remove installed param from default config to proper installation process.

--- site.php
+++ site.php
@@ -1,26 +1,25 @@
-<?php 
+<?php
 return array (
-  'database' => 
+  'database' =>
   array (
     'dsn' => 'mysql://root@localhost/cogear',
   ),
   'permitted_uri_chars' => 'а-яa-z0-9\s_\.\-',
   'key' => 'ba96917974845ff8a6f178c9551c17c7',
-  'site' => 
+  'site' =>
   array (
     'name' => 'cogear',
     'development' => 1,
     'url' => 'cogear.new',
   ),
-  'date' => 
+  'date' =>
   array (
     'format' => 'd M Y, H:i',
   ),
-  'mail' => 
+  'mail' =>
   array (
     'from' => 'admin@cogear.ru',
     'from_name' => 'cogear',
     'signature' => '<p>-----------------------<br/>Best regards<br/>cogear<br/>http://cogear.ru',
   ),
-  'installed' => true,
 );
