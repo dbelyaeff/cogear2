7b7920c5ed5cb9cd081e4320115d0eaaf79fed7e
CuamckuyKot <CuamckuyKot@DELL>
2012-12-18 19:24:34 +0300

    Обнолвена шестерёнка подсветки синтаксиса для работы с Jevix.

--- gears/Code/Gear.php
+++ gears/Code/Gear.php
@@ -9,13 +9,30 @@
  * @link		http://cogear.ru
  */
 class Code_Gear extends Gear {
+    protected $hooks = array(
+        'jevix' => 'hookJevix',
+    );
 
-    public function loadAssets() {
+    /**
+     * Хук парсера Jevix
+     *
+     * Добавляем автоматом тег, чтобы наш код подсвечивался
+     *
+     * @param object $Jevix
+     */
+    public function hookJevix($Jevix){
+         $Jevix->cfgSetTagParamDefault('pre', 'class', 'prettyprint', true);
+    }
+    /**
+     * Переопределяем загрузку скриптов и стилей
+     */
+ /*   public function loadAssets() {
         //parent::loadAssets();
 
         $this->assets->addStyle($this->dir . DS . 'css' . DS . 'prettify.css');
         $this->assets->addScript($this->dir . DS . 'js' . DS . 'prettify.js');
         $this->assets->addScript($this->dir . DS . 'js' . DS . 'script.js');
     }
+*/
 
 }
