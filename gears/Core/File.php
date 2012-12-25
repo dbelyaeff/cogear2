9eefc9a27b9ce03afd2f8c4f29a24ab1314339fa
CuamckuyKot <CuamckuyKot@DELL>
2012-12-25 21:34:21 +0300

    Русификация комментариев.

--- gears/Core/File.php
+++ gears/Core/File.php
@@ -1,16 +1,13 @@
 <?php
 
 /**
- * File Object alias
+ * Алиас для объекта Файл
  *
  * @author		Беляев Дмитрий <admin@cogear.ru>
  * @copyright		Copyright (c) 2010, Беляев Дмитрий
  * @license		http://cogear.ru/license.html
  * @link		http://cogear.ru
- * @package		Core
- * @subpackage
-
  */
 class File extends File_Object {
-    
+
 }
