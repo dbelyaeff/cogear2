0a074764d71c4761bd71b23dec2725ec3402a155
CuamckuyKot <CuamckuyKot@DELL>
2013-01-08 11:06:09 +0300

    Добавление шестерёнки «География». Пока что просто список всех стран с переводом на английский язык.

---
+++ gears/Geo/Gear.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Шестеренка, помогающая крутить Землю
+ *
+ * @author		Беляев Дмитрий <admin@cogear.ru>
+ * @copyright		Copyright (c) 2013, Беляев Дмитрий
+ * @license		http://cogear.ru/license.html
+ * @link		http://cogear.ru
+ */
+class Geo_Gear extends Gear {
+
+}
