ea49b655882e513956a9bf65ff961e4b8a70e422
CuamckuyKot <CuamckuyKot@DELL>
2013-01-09 11:09:03 +0300

    Красивый fieldset с возможностью его свертывания.

---
+++ gears/Form/js/fieldset.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('fieldset').each(function(){
+        $this = $(this);
+        if($this.hasClass('collapsed')){
+            $chevron = $('<i class="icon icon-chevron-right"/>')
+        }
+        else {
+            $chevron = $('<i class="icon icon-chevron-down"/>')
+        }
+        $legend = $this.find('> legend');
+        $legend.append($chevron);
+        $wrapper = $("<div class='wrapper'/>");
+        $legend.after($wrapper);
+        $wrapper.nextAll().appendTo($wrapper);
+        $chevron.on('click',function(){
+            if($this.hasClass('collapsed')){
+                $wrapper.slideDown();
+                $this.removeClass('collapsed');
+                $chevron.removeClass('icon-chevron-right').addClass('icon-chevron-down');
+            }
+            else {
+                $wrapper.slideUp();
+                $this.addClass('collapsed');
+                $chevron.removeClass('icon-chevron-down').addClass('icon-chevron-right');
+            }
+        })
+    })
+})
