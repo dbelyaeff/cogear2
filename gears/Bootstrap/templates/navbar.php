90ecf3709561a6589afca54c92dd33a9d8957e35
CuamckuyKot <CuamckuyKot@DELL>
2013-01-09 19:51:14 +0300

    Замена title на tooltip.

--- gears/Bootstrap/templates/navbar.php
+++ gears/Bootstrap/templates/navbar.php
@@ -2,20 +2,20 @@
         <div class="container">
             <?php if ($brand = $menu->filter(array('place' => 'brand'))): ?>
                 <?php foreach ($brand as $item): ?>
-                    <a class="brand" <?php if($item->title){ echo ' title="'.$item->title.'"';}?> href="<?php echo $item->link; ?>"><?php echo $item->label ?></a>
+                    <a class="brand" <?php if($item->tooltip){ echo ' title="'.$item->tooltip.'"';}?> href="<?php echo $item->link; ?>"><?php echo $item->label ?></a>
                 <?php endforeach; ?>
             <?php endif; ?>
             <?php if ($left = $menu->filter(array('place' => 'left'))): ?>
                 <ul class="nav">
                     <?php foreach ($left as $item): ?>
-                    <li class="<?php if($item->active) echo 'active'?>"><a <?php if($item->title){ echo ' title="'.$item->title.'"';}?>  href="<?php echo $item->link; ?>"><?php echo $item->label ?></a></li>
+                    <li class="<?php if($item->active) echo 'active'?>"><a <?php if($item->tooltip){ echo ' title="'.$item->tooltip.'"';}?>  href="<?php echo $item->link; ?>"><?php echo $item->label ?></a></li>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
             <?php if ($right = $menu->filter(array('place' => 'right'))): ?>
                 <ul class="nav pull-right">
                     <?php foreach ($right as $item): ?>
-                    <li class="<?php if($item->active) echo 'active'?>"><a <?php if($item->title){ echo ' title="'.$item->title.'"';}?>  href="<?php echo $item->link; ?>"><?php echo $item->label ?></a></li>
+                    <li class="<?php if($item->active) echo 'active'?>"><a <?php if($item->tooltip){ echo ' title="'.$item->tooltip.'"';}?>  href="<?php echo $item->link; ?>"><?php echo $item->label ?></a></li>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
