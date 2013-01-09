64bbe97899b5cb4c1033141c82456eb110743fd9
CuamckuyKot <CuamckuyKot@DELL>
2013-01-09 10:40:04 +0300

    Упрощение конфигов форм.

--- gears/Gears/forms/add.php
+++ gears/Gears/forms/add.php
@@ -1,49 +1,43 @@
 <?php
 
 return array(
-    'name' => 'gears.add',
-    'class' => 'form form-horizontal',
-    'elements' => array(
-        'info' => array(
-            'type' => 'div',
-            'class' => 'alert alert-info',
-            'label' => t('Вы можете загрузить одну или несколько шестерёнок в одном архиве. Система автоматически установит их.'),
+    '#name' => 'gears.add',
+    '#class' => 'form form-horizontal',
+    'info' => array(
+        '#type' => 'div',
+        '#class' => 'alert alert-info',
+        '#label' => t('Вы можете загрузить одну или несколько шестерёнок в одном архиве. Система автоматически установит их.'),
+    ),
+    'field' => array(
+        '#type' => 'fieldset',
+        'file' => array(
+            '#label' => t('С диска'),
+            '#type' => 'file',
+            '#allowed_types' => array('zip'),
+            '#maxsize' => 3072,
+            '#path' => UPLOADS . DS . 'gears',
+            '#overwrite' => TRUE,
         ),
-        'field' => array(
-            'type' => 'fieldset',
-            'elements' => array(
-                'file' => array(
-                    'label' => t('С диска'),
-                    'type' => 'file',
-                    'allowed_types' => array('zip'),
-                    'maxsize' => 3072,
-                    'path' => UPLOADS . DS . 'gears',
-                    'overwrite' => TRUE,
-                ),
-                'or' => array(
-                    'type' => 'div',
-                    'value' => '<h2>'.t('ИЛИ').'</h2>',
-                ),
-                'url' => array(
-                    'type' => 'file_url',
-                    'label' => t('С Интернета'),
-                    'class' => 'input-xxxlarge',
-                    'allowed_types' => array('zip'),
-                    'maxsize' => 3072,
-                    'path' => UPLOADS . DS . 'gears',
-                    'overwrite' => TRUE,
-                    'validators' => array('Url'),
-                ),
-            ),
+        'or' => array(
+            '#type' => 'div',
+            '#value' => '<h2>' . t('ИЛИ') . '</h2>',
         ),
-        'actions' => array(
-            'class' => 't_c',
-            'elements' => array(
-                'submit' => array(
-                    'class' => 'btn btn-primary btn-large',
-                    'label' => t('Загрузить'),
-                )
-            )
+        'url' => array(
+            '#type' => 'file_url',
+            '#label' => t('С Интернета'),
+            '#class' => 'input-xxxlarge',
+            '#allowed_types' => array('zip'),
+            '#maxsize' => 3072,
+            '#path' => UPLOADS . DS . 'gears',
+            '#overwrite' => TRUE,
+            '#validators' => array('Url'),
+        ),
+    ),
+    'actions' => array(
+        '#class' => 't_c',
+        'submit' => array(
+            '#class' => 'btn btn-primary btn-large',
+            '#label' => t('Загрузить'),
         )
     )
 );
