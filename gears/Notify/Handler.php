0dfdcdd9ee30f8c6802eae50b4c90770923a897f
CuamckuyKot <CuamckuyKot@DELL>
2013-01-09 14:25:00 +0300

    Добавление метода возврата примечаний.

--- gears/Notify/Handler.php
+++ gears/Notify/Handler.php
@@ -11,14 +11,33 @@
 class Notify_Handler extends Errors_Handler {
 
     protected $notices;
+    /**
+     * Конструктор
+     *
+     * @param array $options
+     * @param int $place
+     */
+    public function __construct($options = array(), $place = 0) {
+        parent::__construct($options, $place);
+        $this->notices = new Core_ArrayObject();
+    }
 
     /**
      * Добавление примечания
      *
-     * @param type $notice
+     * @param string $notice
      */
     public function notice($notice) {
         $this->notices->append($notice);
+    }
+
+    /**
+     * Возвращает примечания
+     *
+     * @return mixed
+     */
+    public function getNotices() {
+        return $this->notices->count() ? $this->notices : NULL;
     }
 
 }
