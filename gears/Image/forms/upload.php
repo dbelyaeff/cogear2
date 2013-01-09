830b15cff56f4fe30a3ecb5008e0e7b469f900cd
CuamckuyKot <CuamckuyKot@DELL>
2013-01-09 10:40:26 +0300

    Упрощение конфигов форм.

--- gears/Image/forms/upload.php
+++ gears/Image/forms/upload.php
@@ -1,22 +1,20 @@
 <?php
 
 return array(
-    'name' => 'image-upload',
-    'ajax' => TRUE,
-    'class' => 'horizontal',
-    'elements' => array(
-        'title' => array(
-            'label' => '<h1>'.t('Загрузка изображений').'</h1>'
-        ),
-        'image' => array(
-            'type' => 'image',
-            'label' => t('Выберите файл с диска'),
-            'maxsize' => '100Kb',
-        ),
-        'dragndrop' => array(
-            'type' => 'div',
-            'class' => 'well t_c',
-            'label' => t('или<p><b>Перетащите его мышкой на это поле.</b>'),
-        ),
+    '#name' => 'image-upload',
+    '#ajax' => TRUE,
+    '#class' => 'horizontal',
+    'title' => array(
+        '#label' => '<h1>' . t('Загрузка изображений') . '</h1>'
+    ),
+    'image' => array(
+        '#type' => 'image',
+        '#label' => t('Выберите файл с диска'),
+        '#maxsize' => '100Kb',
+    ),
+    'dragndrop' => array(
+        '#type' => 'div',
+        '#class' => 'well t_c',
+        '#label' => t('или<p><b>Перетащите его мышкой на это поле.</b>'),
     ),
 );
