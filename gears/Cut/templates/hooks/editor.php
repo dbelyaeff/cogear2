1cdc04c7b9192fd581a887973baba43967a74f66
CuamckuyKot <CuamckuyKot@DELL>
2013-01-10 21:12:33 +0300

    Убрал лишний <br/>.

--- gears/Cut/templates/hooks/editor.php
+++ gears/Cut/templates/hooks/editor.php
@@ -4,9 +4,7 @@
 <script>
     $(document).ready(function(){
         $('#cut-button').on('click',function(){
-            if(0 == $('form textarea:contains("[cut"),form div:contains("[cut")').length){
-                window.insertWysiwyg("\n[cut]<br/>");
-            }
+            window.insertWysiwyg("\n[cut]");
         })
     })
 </script>
