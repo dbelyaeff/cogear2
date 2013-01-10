b121f0c38aee1f08780cca6e1137742831812174
CuamckuyKot <CuamckuyKot@DELL>
2013-01-10 15:13:32 +0300

    Добавлены два объекта для вывода инфомрации до и после элемента.

--- gears/Form/templates/element.php
+++ gears/Form/templates/element.php
@@ -1,7 +1,18 @@
-<div class="control-group <? if($required){?>required<?}?><? if(isset($class)){echo ' '.$class;}?>" id="<?php echo $form->getId()?>-<?php echo $name?>">
-<? if($label){?><label class="control-label" for="<?php echo $name?>"><?php echo $label?><? if($required){?> *<?}?></label><?}?>
-<div class="controls"><?php echo $code?>
-<?php if($errors = $element->getErrors()):?><p class="help-inline"><?php echo $errors->toString('<br/>')?></p><?php endif;?>
-<? if($description){?><div class="description" id="<?php echo $element->getId()?>-description"><?php echo $description?></div><?}?>
+<?php
+echo $before;
+?>
+<div class="control-group <?php if ($required) { ?>required<?php } ?><?php
+if (isset($class)) {
+    echo ' ' . $class;
+}
+?>" id="<?php echo $form->getId() ?>-<?php echo $name ?>">
+        <?php if ($label) { ?><label class="control-label" for="<?php echo $name ?>"><?php echo $label ?><?php if ($required) { ?> *<?php } ?></label><?php } ?>
+    <div class="controls"><?php echo $code ?>
+<?php if ($errors = $element->getErrors()): ?><p class="help-inline"><?php echo $errors->toString('<br/>') ?></p><?php endif; ?>
+<?php if ($description) { ?><div class="description" id="<?php echo $element->getId() ?>-description"><?php echo $description ?></div><?php } ?>
+
+    </div>
 </div>
-</div>
+<?php
+echo $after;
+?>
