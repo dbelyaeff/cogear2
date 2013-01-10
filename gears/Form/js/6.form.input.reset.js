48162d5e71ce721c103e2827e1fba9931bfc3364
CuamckuyKot <CuamckuyKot@DELL>
2013-01-10 14:20:32 +0300

    Bugfixes.

--- gears/Form/js/6.form.input.reset.js
+++ gears/Form/js/6.form.input.reset.js
@@ -1,43 +1,46 @@
 $.fn.resetButton = function(){
-    $el = $(this);
-    $el.wrap($('<div class="element-wrapper"/>'));
-    $wrapper = $el.parent();
-    $reset = $('<i class="icon icon-remove"></i>');
-    $wrapper.css({
-        position:'relative',
-        display: $wrapper.parent('[class$="pend"]').length ? 'inline-block' : 'block'
-    } );
-    $reset.css({
-        position: 'absolute',
-        right: '10px',
-        top: '8px',
-        cursor: 'pointer',
-        opacity: '0.3'
-    })
-    $reset.hover(function(){
-        $(this).css('opacity',1);
-    }, function(){
-        $(this).css('opacity',0.3);
-    });
-    $reset.on('click',function(){
-        $el.val('');
+    $(this).each(function(){
+        var $el = $(this);
+        $el.wrap($('<div class="element-wrapper"/>'));
+        var $wrapper = $el.parent();
+        $wrapper.attr('id',$el.attr('id')+'-wrapper');
+        var $reset = $('<i class="icon icon-remove"></i>');
+        $wrapper.append($reset);
+        $wrapper.css({
+            position:'relative',
+            display: $wrapper.parent('[class$="pend"]').length ? 'inline-block' : 'block'
+        } );
+        $reset.css({
+            position: 'absolute',
+            right: '10px',
+            top: '8px',
+            cursor: 'pointer',
+            opacity: '0.3'
+        })
+        $reset.hover(function(){
+            $(this).css('opacity',1);
+        }, function(){
+            $(this).css('opacity',0.3);
+        });
+        $reset.bind('click',function(){
+            $el.val('');
+            $el.trigger('change')
+            $(this).hide();
+        })
+        $reset.hide();
+        $(this).on('keydown keyup change',function(event){
+            if(event.type == 'keydown' && event.keyCode == 46 && event.ctrlKey == true){
+                $reset.trigger('click')
+            }
+            if($(this).val()){
+                $reset.show();
+            }
+            else {
+                $reset.hide();
+            }
+        });
         $el.trigger('change')
-        $(this).hide();
-    })
-    $reset.hide();
-    $wrapper.append($reset);
-    $el.on('keydown keyup change',function(event){
-        if(event.type == 'keydown' && event.keyCode == 46 && event.ctrlKey == true){
-            $reset.trigger('click')
-        }
-        if($(this).val()){
-            $reset.show();
-        }
-        else {
-            $reset.hide();
-        }
     });
-    $el.trigger('change')
 }
 $(document).ready(function(){
     $('input[type=text], input[type=password], input[type=search]').resetButton();
