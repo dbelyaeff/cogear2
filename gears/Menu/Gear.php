26f24d9224d5d053243234bb8e359d02a6c65b40
CuamckuyKot <CuamckuyKot@DELL>
2013-01-15 00:21:26 +0300

    Удалены экспериментальные особенности.

--- gears/Menu/Gear.php
+++ gears/Menu/Gear.php
@@ -9,35 +9,6 @@
  * @link		http://cogear.ru
  */
 class Menu_Gear extends Gear {
-    protected $hooks = array(
-        'menu.admin.theme' => 'hookMenuAdminTheme',
-    );
-    protected $routes = array(
-      'admin/theme/menu' => 'admin_action',
-      'admin/theme/menu/(\d+)' => 'admin_action',
-    );
-    protected $access = array(
-        'admin' => array(1),
-    );
-    /**
-     * Добавляем пункт меню на страниу админки «Внешний вид»
-     *
-     * @param Menu $menu
-     */
-    public function hookMenuAdminTheme($menu){
-        $menu->add(array(
-            'label' => t('Меню'),
-            'link' => l('/admin/theme/menu'),
-            'title' => FALSE,
-        ));
-    }
 
-    /**
-     * Панель управления меню
-     *
-     * @param type $id
-     */
-    public function admin_action($id = 0){
-        $this->theme->hookAdminMenu();
-    }
+   
 }
