dbefb6d9ae1fcb6187b0df3a1dc133b9ee4d2b96
CuamckuyKot <CuamckuyKot@DELL>
2013-01-15 00:31:31 +0300

    Фикс отсутствующего региона logo.

--- gears/User/templates/login.php
+++ gears/User/templates/login.php
@@ -7,9 +7,7 @@
         <div class="container">
             <div class="row">
                 <div class="span7" id="wrapper">
-                    <p style="margin: 10px 0 0 10px">
-                        <?php echo theme('logo') ?>
-                    </p>
+                    <?php echo theme('header') ?>
                     <div id="content">
                         <?php echo theme('info') ?>
                         <?php echo theme('content') ?>
