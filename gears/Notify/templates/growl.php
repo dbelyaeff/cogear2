2d4a92d3a4de20bda958afa334363a66b0280b28
CuamckuyKot <CuamckuyKot@DELL>
2013-01-14 23:14:32 +0300

    Выводит окошко только после загрузки документа.

--- gears/Notify/templates/growl.php
+++ gears/Notify/templates/growl.php
@@ -1,9 +1,15 @@
 <script type="text/javascript">
-    <?php
-        $options = array();
-        if($title) $options['header'] = addslashes ($title);
-        if($class) $options['theme'] = $class;
-        $options['position'] = config('notify.growl.position','top-right');
-    ?>
-    $.jGrowl("<?php echo addslashes($body)?>"<?php if($options){ echo ', '.json_encode($options);}?>)
+    $(document).ready(function(){
+<?php
+$options = array();
+if ($title)
+    $options['header'] = addslashes($title);
+if ($class)
+    $options['theme'] = $class;
+$options['position'] = config('notify.growl.position', 'top-right');
+?>
+            $.jGrowl("<?php echo addslashes($body) ?>"<?php if ($options) {
+    echo ', ' . json_encode($options);
+} ?>)
+        });
 </script>
