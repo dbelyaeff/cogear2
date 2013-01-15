81b249940b57eff18ba5dca55abb74b5a3d98683
Dmitriy Belyaev <admin@cogear.ru>
2013-01-16 00:33:01 +0400

    Добавление зависимости от шестерёнки Wysiwyg.

--- gears/Redactor/info.php
+++ gears/Redactor/info.php
@@ -3,4 +3,9 @@
 return array(
     'name' => t('Редактор'),
     'description' => t('Продвинутый визуальный редактор'),
+    'required' => array(
+        'gears' => array(
+            array('name' => 'Wysiwyg')
+        )
+    )
 );
