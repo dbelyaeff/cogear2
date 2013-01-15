cea0797989ee29991ca842a57176cf4225407aec
CuamckuyKot <CuamckuyKot@DELL>
2013-01-15 17:41:50 +0300

    Улучшен скрипт навигационной штуки.

--- gears/Pager/js/pager.js
+++ gears/Pager/js/pager.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    $(window).bind('keyup.pager',function(e){
+    $(window).on('keydown.pager',function(e){
         if(e.ctrlKey && e.keyCode == 37){
             var prev = $('.pagination .prev');
             if(prev.length){
-                    document.location = prev.attr('href');
+                    document.location.href = prev.attr('href');
             }
         }
         if(e.ctrlKey && e.keyCode == 39){
             var next = $('.pagination .next');
             if(next.length){
-                    document.location = next.attr('href');
+                    document.location.href = next.attr('href');
             }
         }
     })
