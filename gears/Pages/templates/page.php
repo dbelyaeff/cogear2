c042e0c89a2ddcf07299b8c4b4eaf3bd174bcdc2
CuamckuyKot <CuamckuyKot@DELL>
2013-01-15 09:57:16 +0300

    Обновление шаблона страницы.

--- gears/Pages/templates/page.php
+++ gears/Pages/templates/page.php
@@ -1,1 +1,9 @@
-<?php echo $item->name?>
+<?php
+event('parser', $item);
+?>
+<article class="page">
+<h1><?php echo $item->name ?></h1>
+<div class="page-body">
+    <?php echo $item->body?>
+</div>
+</article>
