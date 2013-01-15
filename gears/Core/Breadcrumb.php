dc3a6bdd7f9e516397f530e95fa8c352efcde672
CuamckuyKot <CuamckuyKot@DELL>
2013-01-15 14:35:13 +0300

    Добавлен алиас для класса Breadcrumb

---
+++ gears/Core/Breadcrumb.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Алиас для класса Breadcrumb_Object
+ *
+ * @author		Беляев Дмитрий <admin@cogear.ru>
+ * @copyright		Copyright (c) 2013, Беляев Дмитрий
+ * @license		http://cogear.ru/license.html
+ * @link		http://cogear.ru
+ */
+class Breadcrumb extends Breadcrumb_Object {
+
+}
