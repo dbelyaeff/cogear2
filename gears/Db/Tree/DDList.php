24ad39d8441fb64ccdea62462fd2cb877a30159f
Dmitriy Belyaev <admin@cogear.ru>
2013-01-17 00:02:44 +0400

    Немного пофиксен драг-дроп.

--- gears/Db/Tree/DDList.php
+++ gears/Db/Tree/DDList.php
@@ -12,6 +12,7 @@ class Db_Tree_DDList extends Options {
 
     protected $options = array(
         'class' => '',
+        'items' => '',
         'render' => 'content',
         'template' => 'Db/templates/tree/dd',
         'saveUri' => '/db/saveDDtree',
@@ -26,21 +27,28 @@ class Db_Tree_DDList extends Options {
         parent::__construct($options);
         $this->render && hook($this->render, array($this, 'show'));
     }
+
     /**
      * Вывод
      */
-    public function render(){
+    public function render() {
 
-        if(!class_exists($this->options->class)){
-            return error(t('Класс <b>%s</b> не существует.',$this->options->class));
+        $items = array();
+        if ($this->options->class) {
+            if (!class_exists($this->options->class)) {
+                return error(t('Класс <b>%s</b> не существует.', $this->options->class));
+            }
+            $object = new $this->options->class();
+            $items = $object->findAll();
+        } elseif ($this->options->items) {
+            $items = $this->options->items;
         }
-        $object = new $this->options->class();
-        if($items = $object->findAll()){
+        if ($items) {
             $template = new Template($this->template);
             $template->options = $this->options;
             $template->items = $items;
-            js(cogear()->db->folder.'/js/inline/jquery.nestable.js');
-            js(cogear()->db->folder.'/js/inline/nest.js');
+            js(cogear()->db->folder . '/js/inline/jquery.nestable.js');
+            js(cogear()->db->folder . '/js/inline/nest.js');
             return $template->render();
         }
     }
