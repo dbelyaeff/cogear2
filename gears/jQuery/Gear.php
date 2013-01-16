67cfa41225d1d8bf182a345496fbda4dc1c88ee8
Dmitriy Belyaev <admin@cogear.ru>
2013-01-16 22:41:11 +0400

     Перенос jQuery и jQuery UI на загрузку с CDN. Во-первых, так быстрее. Во-вторых, экономится место в дистрибутиве и время загрузки.

--- gears/jQuery/Gear.php
+++ gears/jQuery/Gear.php
@@ -9,13 +9,19 @@
  * @link		http://cogear.ru
  */
 class jQuery_Gear extends Gear {
+    protected $hooks = array(
+        'assets.js.global' => 'hookjQuery',
+    );
 
+    public function hookjQuery(){
+        echo HTML::script('http://code.jquery.com/jquery-1.8.3.js');
+    }
+//
 }
 /**
  * Подключает jQuery UI
  */
 function jqueryui(){
-    $folder = cogear()->jQuery->folder.'/UI/';
-    js($folder.'jquery-ui-1.9.2.custom.min.js');
-    css($folder.'jquery-ui-1.9.2.custom.min.css');
+    //css('http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css');
+    js('http://code.jquery.com/ui/1.9.2/jquery-ui.js');
 }
