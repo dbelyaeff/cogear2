3dcebacf7d485102707368635df0436b18f24b36
Dmitriy Belyaev <admin@cogear.ru>
2013-01-17 11:11:17 +0400

    Добавил волшебные методы.

--- gears/Input/Cookie.php
+++ gears/Input/Cookie.php
@@ -1,46 +1,74 @@
 <?php
+
 /**
- *  Cookie
+ *  Куки
  *
  * @author		Беляев Дмитрий <admin@cogear.ru>
  * @copyright		Copyright (c) 2011, Беляев Дмитрий
  * @license		http://cogear.ru/license.html
  * @link		http://cogear.ru
- * @package		Core
- * @subpackage
-
  */
 class Input_Cookie {
+
     /**
-     * Set cookie
+     * Ставит куку
      *
      * @param string $name
      * @param string $value
      * @param int $expire
      * @param string $path
      */
-    public static function set($name,$value,$expire = NULL,$path = NULL,$domain = NULL){
+    public static function set($name, $value, $expire = NULL, $path = NULL, $domain = NULL) {
         // By default cookie lifetime is month
         $expire OR $expire = 2592000;
         $path OR $path = '/';
-        $domain OR $domain =  strpos(SITE_URL,'.') !== FALSE ? '.'.SITE_URL : SITE_URL;
-        setcookie($name, $value, time()+$expire, $path, $domain);
+        $domain OR $domain = self::getDomain();
+        setcookie($name, $value, time() + $expire, $path, $domain);
     }
     /**
-     * Get cookie
+     * Волшебный метод для установки куки
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __set($name,$value){
+        return self::set($name,$value);
+    }
+    /**
+     * Получает куку
      *
      * @param string $name
      * @return string
      */
-    public static function get($name){
-        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : NULL;
+    public static function get($name) {
+        return isset($_COOKIE[$name]) ? unserialize($_COOKIE[$name]) : NULL;
     }
     /**
-     * Delete cookie
+     * Волшебный метод для получения куки
+     *
+     * @param type $name
+     * @return type
+     */
+    public function __get($name){
+        return self::get($name);
+    }
+
+    /**
+     * Удаляет куку
      *
      * @param string $name
      */
-    public static function delete($name){
-        self::set($name,NULL,-1);
+    public static function delete($name) {
+        self::set($name, NULL, -1);
     }
+
+    /**
+     * Возвращает домен для кукисов
+     *
+     * @return string
+     */
+    public static function getDomain() {
+        return strpos(SITE_URL, '.') ? '.' . SITE_URL : '';
+    }
+
 }
