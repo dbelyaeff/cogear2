33ca084c65a80f525a3e2467aec8afa748f278d7
Dmitriy Belyaev <admin@cogear.ru>
2013-01-23 17:45:24 +0400

    Шестерёнка Parser несовместима с установщиком.

--- gears/Parser/info.php
+++ gears/Parser/info.php
@@ -4,4 +4,15 @@ return array(
     'name' => t('Парсер'),
     'description' => t('Фильтрация HTML-кода.'),
     'order' => '0',
+    'required' =>
+    array(
+        'gears' =>
+        array(
+            0 =>
+            array(
+                'name' => 'Install',
+                'disabled' => 'TRUE',
+            ),
+        ),
+    ),
 );
