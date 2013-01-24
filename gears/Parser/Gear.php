f57f4b7f529895c6b73e00cafcc68323724801fd
Dmitriy Belyaev <admin@cogear.ru>
2013-01-24 12:13:11 +0400

    Реализована панель управления парсером. Добавлена возможность автоматической обработки перевода строк.

--- gears/Parser/Gear.php
+++ gears/Parser/Gear.php
@@ -12,9 +12,14 @@ class Parser_Gear extends Gear {
 
     protected $hooks = array(
         'form.load' => 'hookFormLoad',
+        'parse' => 'hookParse',
     );
     protected $access = array(
         'off' => array(1),
+        'admin' => array(1),
+    );
+    protected $routes = array(
+        'admin/parser' => 'admin_action',
     );
     /**
      * Конструтктор
@@ -24,7 +29,19 @@ class Parser_Gear extends Gear {
     public function __construct($config) {
         parent::__construct($config);
     }
-
+    /**
+     * Хук парсера
+     *
+     * @param type $item
+     */
+    public function hookParse($item){
+        // Автоматический парсинг перевода строки
+        if(config('Parser.nl2br') && $item->body){
+            $item->body = preg_replace('#\>([\n\r\t\s]+)\<#imsU','><',$item->body);
+            $item->body = preg_replace('#\<br/?\>#imsU',"",$item->body);
+            $item->body = nl2br($item->body);
+        }
+    }
     /**
      * Хук инициализации формы
      *
@@ -44,5 +61,28 @@ class Parser_Gear extends Gear {
                 ));
             }
         }
+    }
+    /**
+     * Настройки
+     */
+    public function admin_action(){
+        $form = new Form(array(
+            '#name' => 'admin.parser',
+            'title' => array(
+                'label' => t('Настройки'),
+            ),
+            'nl2br' => array(
+                'type' => 'checkbox',
+                'label' => t('Автоматическая обработка строк'),
+                'value' => config('Parser.nl2br'),
+            ),
+            'save' => array()
+        ));
+        if($result = $form->result()){
+            $this->set('Parser.nl2br',$result->nl2br);
+            flash_success(t('Настройки сохранены!'));
+            reload();
+        }
+        $form->show();
     }
 }
