e6deb446d4c62de919cb32ff881c9b4638561ae6
Dmitriy Belyaev <admin@cogear.ru>
2013-02-10 19:14:27 +0400

    Исправление опечатки в строке перевода.

--- gears/File/Element.php
+++ gears/File/Element.php
@@ -42,7 +42,7 @@ class File_Element extends Form_Element_Abstract {
             $this->notice(t('Следующие типы файлов разрешены к загрузке: <b>%s</b>.', $this->options->allowed_types->toString('</b>, <b>')));
         }
         if ($this->options->maxsize) {
-            $this->notice(t('Макстмальный размер файла <b>%s</b>.', File::fromBytes(File::toBytes($this->options->maxsize), NULL, 2)));
+            $this->notice(t('Максимальный размер файла <b>%s</b>.', File::fromBytes(File::toBytes($this->options->maxsize), NULL, 2)));
         }
         if ($this->notices->count()) {
             $this->options->description .= '<ul class="file-notice"><li>' . $this->notices->toString('</li><li>') . '</li></ul>';
