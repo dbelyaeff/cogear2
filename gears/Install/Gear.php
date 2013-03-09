c06b1f2cdf58dad1743dbf1a5b19c6e232f6f81c
CuamckuyKot <admin@cogear.ru>
2013-03-09 20:44:21 +0400

    Исправлена работа установщика.

--- gears/Install/Gear.php
+++ gears/Install/Gear.php
@@ -47,30 +47,30 @@ class Install_Gear extends Gear {
     public function index($action = '') {
         template('Install/templates/header')->show('info');
         new Menu_Tabs(array(
-                    'name' => 'install',
-                    'render' => 'content',
-                    'elements' => array(
-                        array(
-                            'label' => t('1. Начало'),
-                            'link' => '',
-                            'active' => check_route('install$'),
-                        ),
-                        array(
-                            'label' => t('2. Проверка'),
-                            'link' => '',
-                            'active' => check_route('check$'),
-                        ),
-                        array(
-                            'label' => t('3. Настройки'),
-                            'link' => '',
-                            'active' => check_route('site$'),
-                        ),
-                        array(
-                            'label' => t('4. Завершение'),
-                            'link' => '',
-                            'active' => check_route('finish$'),
-                        ),
-                    ),
+            'name' => 'install',
+            'render' => 'content',
+            'elements' => array(
+                array(
+                    'label' => t('1. Начало'),
+                    'link' => '',
+                    'active' => check_route('install$'),
+                ),
+                array(
+                    'label' => t('2. Проверка'),
+                    'link' => '',
+                    'active' => check_route('check$'),
+                ),
+                array(
+                    'label' => t('3. Настройки'),
+                    'link' => '',
+                    'active' => check_route('site$'),
+                ),
+                array(
+                    'label' => t('4. Завершение'),
+                    'link' => '',
+                    'active' => check_route('finish$'),
+                ),
+            ),
                 ));
         switch ($action) {
             case 'check':
@@ -82,12 +82,12 @@ class Install_Gear extends Gear {
                 $form = new Form('Install/forms/site');
 
                 if ($result = $form->result()) {
-                    $config = new Config(SITE . DS . 'site' . EXT);
+                    $site = new Config(SITE . DS . 'site' . EXT);
+                    $config = new Config(SITE . DS . 'config' . EXT);
                     $config->site->name = $result->sitename;
-                    $config->site->url = str_replace(array('http://','www'),'',trim($result->sitehost,'/'));
-                    $config->key OR $config->key = md5(md5(time()) + time() + $config->site->name);
+                    $site->key OR $site->key = md5(md5(time()) + time() + $site->site->name);
                     $result->port OR $result->port = 3306;
-                    $config->database = array(
+                    $site->database = array(
                         'driver' => config('database.driver'),
                         'host' => $result->host,
                         'base' => $result->base,
@@ -96,21 +96,24 @@ class Install_Gear extends Gear {
                         'port' => $result->port,
                         'prefix' => $result->prefix,
                     );
-                    $db = Db::factory('temp', $config->database);
+                    $db = Db::factory('temp', $site->database);
                     if (!$db->connect()) {
                         if ($result->create_db && $db->connect(FALSE)) {
-                            $db->query("CREATE DATABASE `{$config->database->base}` DEFAULT  CHARACTER SET utf8 COLLATE utf8_general_ci;
-                CREATE USER '{$config->database->user}'@'{$config->database->host}' IDENTIFIED BY '{$config->database->pass}';
-                GRANT ALL ON `{$config->database->base}`.* TO '{$config->database->user}'@'localhost';
+                            $db->query("CREATE DATABASE `{$site->database->base}` DEFAULT  CHARACTER SET utf8 COLLATE utf8_general_ci;
+                CREATE USER '{$site->database->user}'@'{$site->database->host}' IDENTIFIED BY '{$site->database->pass}';
+                GRANT ALL ON `{$site->database->base}`.* TO '{$site->database->user}'@'localhost';
                 FLUSH PRIVILEGES;");
                         }
                         $db->connect();
                     }
-                    if ($db->is_connected && $db->import($this->dir . DS . 'cogear.sql')) {
+                    if ($db->is_connected) {
+                        $site->store(TRUE);
                         $config->store(TRUE);
-                        redirect(l('/install/finish'));
+                        if ($db->import($this->dir . DS . 'cogear.sql',$site->database->prefix)) {
+                            redirect(l('/install/finish'));
+                        }
                     } else {
-                        error(t("Не удалось установить подключение к базе данных."),'','content');
+                        error(t("Не удалось установить подключение к базе данных."), '', 'content');
                     }
                 } else {
                     $form->save->label = t('Попробуйте снова');
@@ -122,9 +125,9 @@ class Install_Gear extends Gear {
                 $tpl->show();
                 break;
             case 'done':
-                $config = new Config(SITE . DS . 'site' . EXT);
-                $config->store(TRUE);
-                flash_success(t('Ваш сайт успешно настроен! <p> Данные для входа – логин <b>admin</b> и пароль <b>password</b>.'),'','info');
+//                $site = new Config(SITE . DS . 'site' . EXT);
+//                $site->store(TRUE);
+                flash_success(t('Ваш сайт успешно настроен! <p> Данные для входа – логин <b>admin</b> и пароль <b>password</b>.'), '', 'info');
                 $this->disable();
                 redirect();
                 break;
