2536f4b81a0eeab098098b7a3ec380a69f499ce6
CuamckuyKot <admin@cogear.ru>
2013-03-09 21:18:19 +0400

    Добавлена кнопка-фильтр тех шестерёнок, которые требуют обновления.

--- gears/Gears/templates/table.php
+++ gears/Gears/templates/table.php
@@ -1,10 +1,10 @@
 <form action="<?php echo l('/admin/gears/status/') ?>" method="POST">
-    <div class="input-append"><input type="search" class="input-xxlarge" placeholder="<?php echo t("Начните ввод для фильтрации…"); ?>" id="search-gears" tabindex="0"/><a href="#filter" class="btn" onclick="$('#search-gears').val('<?php echo t('Настройки')?>').trigger('change')"><i class="icon icon-cogs"></i></a></div>
-    <?php echo template('Gears/templates/formaction',array('do' => 'do'))->render() ?>
+    <div class="input-append"><input type="search" class="input-xxlarge" placeholder="<?php echo t("Начните ввод для фильтрации…"); ?>" id="search-gears" tabindex="0"/><a href="#search-gears" class="btn" onclick="$('#search-gears').val('<?php echo t('Настройки') ?>').trigger('change')"><i class="icon icon-cogs"></i></a> <a href="#search-gears" class="btn btn-primary" onclick="$('#search-gears').val('<?php echo t('Обновить') ?>').trigger('change')"><i class="icon icon-refresh"></i></a></div>
+    <?php echo template('Gears/templates/formaction', array('do' => 'do'))->render() ?>
     <table class="table table-bordered table-hover" id="gears-table">
         <thead>
             <tr>
-                <th width="3%" class="t_c"><input type="checkbox" onclick="$('#gears-table tr:visible input[type=checkbox]:not(:disabled)').attr('checked',this.checked)"></th>
+                <th width="3%" class="t_c"><input type="checkbox" onclick="$('#gears-table tr:visible input[type=checkbox]:not(:disabled)').attr('checked', this.checked)"></th>
                 <th width="15%"><?php echo t('Название') ?></th>
                 <th  width="45%"><?php echo t('Описание') ?></th>
                 <th  width="20%"><?php echo t('Действия') ?></th>
@@ -73,10 +73,13 @@
                             <a href="<?php echo l('/admin/gears/status') . e(array('do' => 'disable', 'gears' => $gear->gear)) ?>"  class="btn btn-mini btn-danger"><?php echo t('Выключить') ?></a>
                         <?php endif; ?>
                         <?php if (method_exists($gear, 'admin_action') && $gear->status() != Gears::DISABLED): ?>
-                            <a href="<?php echo l('/admin/' . strtolower($gear->gear)) ?>"  class="btn btn-mini"><?php echo t('Настройки')?></a>
+                            <a href="<?php echo l('/admin/' . strtolower($gear->gear)) ?>"  class="btn btn-mini"><?php echo t('Настройки') ?></a>
+                        <?php endif; ?>
+                        <?php if (TRUE === $gear->checkUpdate()): ?>
+                            <a href="<?php echo l('/admin/gears/update/' . strtolower($gear->gear)) ?>"  class="btn btn-mini btn-primary"><?php echo t('Обновить') ?></a>
                         <?php endif; ?>
                         <?php if ($gear->status() !== Gears::CORE): ?>
-                            <a href="<?php echo l('/admin/gears/download').'?gears='.$gear->gear?>" class="btn btn-mini" title="<?php echo t('Скачать')?>"><?php echo icon('download')?></a>                      <?php endif; ?>
+                            <a href="<?php echo l('/admin/gears/download') . '?gears=' . $gear->gear ?>" class="btn btn-mini" title="<?php echo t('Скачать') ?>"><?php echo icon('download') ?></a>                      <?php endif; ?>
                     </td>
                 </tr>
             <?php endforeach; ?>
@@ -84,7 +87,7 @@
 
         <tfoot>
             <tr>
-                <th width="3%" class="t_c"><input type="checkbox" onclick="$('#gears-table tr:visible input[type=checkbox]:not(:disabled)').attr('checked',this.checked)"></th>
+                <th width="3%" class="t_c"><input type="checkbox" onclick="$('#gears-table tr:visible input[type=checkbox]:not(:disabled)').attr('checked', this.checked)"></th>
                 <th width="10%"><?php echo t('Название') ?></th>
                 <th  width="50%"><?php echo t('Описание') ?></th>
                 <th  width="20%"><?php echo t('Действия') ?></th>
@@ -92,30 +95,30 @@
         </tfoot>
 
     </table>
-    <?php echo template('Gears/templates/formaction',array('do'=>'do-alt'))->render() ?>
+    <?php echo template('Gears/templates/formaction', array('do' => 'do-alt'))->render() ?>
 </form>
 <script>
-    $(document).ready(function(){
-        $('#search-gears').focus();
-    })
-    function filterTable(value){
-        if(value){
-            $('#gears-table tbody tr:not(:contains("'+value+'"))').hide();
-            $('#gears-table tbody  tr:contains("'+value+'")').show();
+        $(document).ready(function() {
+            $('#search-gears').focus();
+        })
+        function filterTable(value) {
+            if (value) {
+                $('#gears-table tbody tr:not(:contains("' + value + '"))').hide();
+                $('#gears-table tbody  tr:contains("' + value + '")').show();
+            }
+            else {
+                $('#gears-table tbody tr').show();
+            }
         }
-        else {
-            $('#gears-table tbody tr').show();
-        }
-    }
-    $('#search-gears').bind({
-        keyup: function(){
-            filterTable($(this).val());
-        },
-        change: function(){
-            filterTable($(this).val());
-        },
-        clearFields: function(){
-            filterTable($(this).val());
-        }
-    })
+        $('#search-gears').bind({
+            keyup: function() {
+                filterTable($(this).val());
+            },
+            change: function() {
+                filterTable($(this).val());
+            },
+            clearFields: function() {
+                filterTable($(this).val());
+            }
+        })
 </script>
